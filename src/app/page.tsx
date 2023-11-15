8f898af48d951edd43d30c9a5b7aa9e6ecb887ea
Shane Reid <shane@ivodigital.com>
2023-11-15 09:33:57 +0000

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,7 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { MeetTheTeam } from '@/components/MeetTheTeam'
-import { Pricing } from '@/components/Pricing'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import { Testimonials } from '@/components/Testimonials'
 
 export default function Home() {
   return (
