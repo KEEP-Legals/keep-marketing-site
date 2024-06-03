26fc033d816835dfa6272525c733d8f34577af21
Karol Kosek <karol.kosek@deployed.pl>
2024-06-03 17:46:45 +0200

    Pricing componenent -make adjustments

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -56,52 +56,34 @@ function Plan({
   description,
   href,
   features,
-  featured = false,
 }: {
   name: string
   price: string
   description: string
   href: string
   features: Array<string>
-  featured?: boolean
 }) {
   return (
-    <section
-      className={clsx(
-        'flex flex-col rounded-3xl px-6 sm:px-8',
-        featured ? 'order-first bg-blue-600 py-8 lg:order-none' : 'lg:py-8',
-      )}
-    >
-      <h3 className="mt-5 font-display text-lg text-white">{name}</h3>
-      <p
-        className={clsx(
-          'mt-2 text-base',
-          featured ? 'text-white' : 'text-slate-400',
-        )}
-      >
-        {description}
-      </p>
-      <p className="order-first font-display text-5xl font-light tracking-tight text-white">
+    <section className="order-first flex flex-col rounded-3xl bg-white px-6 py-8 text-gray-900 sm:px-8 lg:order-none">
+      <h3 className="mt-5 font-display text-lg ">{name}</h3>
+      <p className="mt-2 text-base">{description}</p>
+      <p className="order-first font-display text-5xl font-light tracking-tight ">
         {price}
       </p>
       <ul
         role="list"
-        className={clsx(
-          'order-last mt-10 flex flex-col gap-y-3 text-sm',
-          featured ? 'text-white' : 'text-slate-200',
-        )}
+        className="order-last mt-10 flex flex-col gap-y-3 text-sm"
       >
         {features.map((feature) => (
           <li key={feature} className="flex">
-            <CheckIcon className={featured ? 'text-white' : 'text-slate-400'} />
+            <CheckIcon />
             <span className="ml-4">{feature}</span>
           </li>
         ))}
       </ul>
       <Button
         href={href}
-        variant={featured ? 'solid' : 'outline'}
-        color="white"
+        variant="solid"
         className="mt-8"
         aria-label={`Get started with the ${name} plan for ${price}`}
       >
@@ -116,7 +98,7 @@ export function Pricing() {
     <section
       id="pricing"
       aria-label="Pricing"
-      className="bg-slate-900 py-20 sm:py-32"
+      className="bg-primary-700 py-20 sm:py-32"
     >
       <Container>
         <div className="md:text-center">
@@ -127,7 +109,7 @@ export function Pricing() {
             </span>{' '}
             for everyone.
           </h2>
-          <p className="mt-4 text-lg text-slate-400">
+          <p className="mt-4 text-lg text-white">
             It doesn’t matter what size your business is, our software won’t
             work well for you.
           </p>
@@ -147,7 +129,6 @@ export function Pricing() {
             ]}
           />
           <Plan
-            featured
             name="Small business"
             price="$15"
             description="Perfect for small / medium sized businesses."
