34d2b8bf4cb1965f513591d0d2ca16d193e63b04
Karol Kosek <karol.kosek@deployed.pl>
2024-06-04 13:24:41 +0200

    Add Video tag to how-it-works page

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -1,12 +1,14 @@
 import { HowItWorksSections } from '@/components/HowItWorksSections'
 import { ArtistManagement } from '@/components/ArtistManagement'
 import { WrapperWithTitle } from '@/components/WrapperWithTitle'
+import { ExplainWhatKeepIsVideo } from '@/components/ExplainWhatKeepIsVideo'
 
 export default function HowItWorks() {
   return (
     <>
       <main>
         <WrapperWithTitle title="What can KEEP do for me">
+          <ExplainWhatKeepIsVideo />
           <HowItWorksSections />
           <ArtistManagement />
         </WrapperWithTitle>
---
+++ src/components/ExplainWhatKeepIsVideo.tsx
@@ -0,0 +1,17 @@
+import { Container } from './Container'
+
+export function ExplainWhatKeepIsVideo() {
+  return (
+    <Container>
+      <figure className="py-2 md:p-16">
+        <video controls preload="auto" width="100%" height="auto">
+          <source
+            src="/images/how-it-works/KEEP_Animation_V.3.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag
+        </video>
+      </figure>
+    </Container>
+  )
+}
