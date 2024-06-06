ccc67cddbd82891aa17cb71e91073bb4276a0107
Karol Kosek <karol.kosek@deployed.pl>
2024-06-06 14:17:14 +0200

    chore: Update video tag in ExplainWhatKeepIsVideo component

--- src/components/ExplainWhatKeepIsVideo.tsx
+++ src/components/ExplainWhatKeepIsVideo.tsx
@@ -4,8 +4,16 @@ export function ExplainWhatKeepIsVideo() {
   return (
     <Container>
       <figure className="py-2 md:p-16">
-        <video controls preload="auto" width="100%" height="auto">
-          <source src="/videos/how-it-works-keep-animation.mp4" type="video/mp4" />
+        <video
+          controls
+          autoPlay
+          loop
+          playsInline
+          preload="meta"
+          width="100%"
+          height="auto"
+          src="/videos/how-it-works-keep-animation.mp4"
+        >
           Your browser does not support the video tag
         </video>
       </figure>
