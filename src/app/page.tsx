e56426d2e47090c1e3276f7329b831d9eae19235
Karol Kosek <karol.kosek@deployed.pl>
2024-06-10 11:05:46 +0200

    Preload hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import { Faqs } from '@/components/Faqs'
 import { Hero } from '@/components/Hero'
 import { JustEmailForm } from '@/components/JustEmailForm'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link as="image" rel="preload" href="/images/hero/hero.webp" />
+      </Head>
       <main>
         <Hero />
         <PrimaryFeatures />
