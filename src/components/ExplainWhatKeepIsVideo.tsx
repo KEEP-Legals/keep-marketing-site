9f66fbe0000d1ecb64d7c6e3d70960e9ef3a2496
Karol Kosek <karol.kosek@deployed.pl>
2024-06-10 11:45:09 +0200

    how-it-works - adjust video

--- src/components/ExplainWhatKeepIsVideo.tsx
+++ src/components/ExplainWhatKeepIsVideo.tsx
@@ -3,7 +3,7 @@ import { Container } from '@/components/Container'
 export function ExplainWhatKeepIsVideo() {
   return (
     <video
-      className="z-100 bg-video-background relative top-0 w-full"
+      className="z-100 bg-video-background max-h-[calc(90vh] relative top-0 w-full max-h-[calc(100vh-330px)] object-contain"
       autoPlay
       loop
       muted
