d3546610c03f38a15f1dc69b5c194fb0a48a5e27
Karol Kosek <karol.kosek@deployed.pl>
2024-06-10 12:59:27 +0200

    Make video full blead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Faqs } from '@/components/Faqs'
 import { Hero } from '@/components/Hero'
 import { JustEmailForm } from '@/components/JustEmailForm'
 import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import Head from 'next/head'
 
 export default function Home() {
   return (
